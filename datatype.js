ef55a19a1e5bf344e89b4e9238f0b273236699e7
prerana30 <preranabxworkz@gmail.com>
2026-03-16 21:42:35 +0530

    first commit

---
+++ datatype.js
@@ -0,0 +1,30 @@
+console.log(typeof "hello");
+console.log(typeof 307898968);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof []);
+
+
+
+console.log('5' + 3);
+
+console.log('5' - 2);
+
+console.log(true + 1);
+console.log(false + 1);
+console.log('' == false);
+console.log(0 == false);
+
+console.log(5 == 5);
+console.log('5' === 5);
+
+
+// explicit
+console.log(typeof String(42));
+console.log(parseInt('42px'));
+console.log(Boolean(''));
+console.log(Boolean(null));
+console.log(Boolean('42'));
+console.log(Number(''));
+console.log((42).toString());
---
+++ variables.js
@@ -0,0 +1,12 @@
+var a= 20;
+let b = 23.0;
+const c = 34; // we can not change the value -- block and function scope
+
+   a = 30; // we can re-Initialize the value --- global scope
+
+   b = 'prerana' // we can re-Intialize and also we can change the datatype -- block and function scope
+ 
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(typeof(a)); // for which data type is using
